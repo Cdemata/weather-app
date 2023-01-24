76bcc8b67c072a07233931930fa0a22634803132
Cdemata <chanelle.demata@gmail.com>
2023-01-24 01:23:02 -0800

    Updated weather condition, humidity, windspeed, date and time,

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-let now = new Date();
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-console.log(now);
-
-let h2 = document.querySelector("h2");
-h2.innerHTML = `${day} ${hours}:${minutes}`;
 
 function showTemperature(response) {
   let cityTemperature = Math.round(response.data.main.temp);
   let temperature = document.querySelector("#temperature");
+  let conditionElement = document.querySelector("h3");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("h2");
+
   temperature.innerHTML = `${cityTemperature}`;
+  conditionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function search(event) {
